245db10a239ab133ec3bdf630a8451be07983abc
TrueMerc <true_merc@mail.ru>
2021-03-25 21:13:09 +0300

    Add an initial project structure

---
+++ src/test/java/ru/ryabtsev/devobserver/DevObserverApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ryabtsev.devobserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevObserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

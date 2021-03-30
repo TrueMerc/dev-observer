ae1ea3fb5542e7e11482122a31c004762450f559
TrueMerc <true_merc@mail.ru>
2021-03-30 21:17:28 +0300

    Add 'Hello, World!'

---
+++ src/main/java/ru/ryabtsev/devobserver/configurations/ApplicationConfiguration.java
@@ -0,0 +1,8 @@
+package ru.ryabtsev.devobserver.configurations;
+
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApplicationConfiguration extends WebMvcAutoConfiguration {
+}

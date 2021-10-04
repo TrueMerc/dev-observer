24362346d7a169ba5642f26d7af043dd0f5ad692
TrueMerc <true_merc@mail.ru>
2021-10-04 14:22:15 +0300

    Add script which create DB tables

---
+++ src/main/resources/db/migration/V1__create_tables.sql
@@ -0,0 +1,20 @@
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+    id BIGSERIAL PRIMARY KEY,
+    login VARCHAR (50) NOT NULL UNIQUE,
+    password VARCHAR (80) NOT NULL,
+    email VARCHAR (50),
+    name VARCHAR (50),
+    surname VARCHAR (50),
+    patronymic VARCHAR (50),
+    role VARCHAR(50)
+);
+
+DROP TABLE IF EXISTS firmware_files;
+CREATE TABLE firmware_files (
+    id BIGSERIAL PRIMARY KEY,
+    original_name VARCHAR (100),
+    name VARCHAR (50),
+    author_id BIGINT,
+    path VARCHAR (300)
+);

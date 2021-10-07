e0a3e981fd6463495360297971dd5786d6fbfda5
TrueMerc <true_merc@mail.ru>
2021-10-07 21:12:53 +0300

    Add tabs for different administration modes

--- src/main/js/app.js
+++ src/main/js/app.js
@@ -4,6 +4,7 @@ import Application from "./components/Application.jsx";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'react-tippy/dist/tippy.css';
 
+
 ReactDOM.render(
     <Application/>,
     document.getElementById("react")
--- src/main/js/components/Administration.jsx
+++ src/main/js/components/Administration.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import "react-tabs/style/react-tabs.css";
+import {Tab, TabList, TabPanel, Tabs} from "react-tabs";
 
 export const Administration = (props) => {
     return  (
-      <h1>Администрирование</h1>
+        <div className='main-area'>
+            <Tabs forceRenderTabPanel>
+                <TabList>
+                    <Tab>Управление пользователями</Tab>
+                    <Tab>Управление устройством</Tab>
+                </TabList>
+
+                <TabPanel>
+                    <h2>Управление пользователями</h2>
+                </TabPanel>
+                <TabPanel>
+                    <h2>Управление устройством</h2>
+                </TabPanel>
+            </Tabs>
+        </div>
+
     );
 }

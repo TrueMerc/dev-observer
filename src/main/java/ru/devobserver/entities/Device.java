c1ea4fecdf753818ff2ec70d0797f6f8ebd3de69
TrueMerc <true_merc@mail.ru>
2022-04-14 21:27:55 +0300

    Add device settings deserialization throug interface

--- src/main/java/ru/devobserver/entities/Device.java
+++ src/main/java/ru/devobserver/entities/Device.java
@@ -2,6 +2,8 @@ package ru.devobserver.entities;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.Type;
+import ru.devobserver.domain.devices.uart.DeviceSettings;
 
 import javax.persistence.*;
 
@@ -10,7 +12,6 @@ import javax.persistence.*;
 @Getter
 @Setter
 public class Device {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -20,6 +21,10 @@ public class Device {
 
     @Column(name = "description")
     private String description;
+
+    @Column(name = "settings", columnDefinition = "jsonb")
+    @Type(type = "jsonb")
+    private DeviceSettings deviceSettings;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "mode")

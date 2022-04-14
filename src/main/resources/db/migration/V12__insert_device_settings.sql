c816be6f4a74886e4a5b4cbafee4fc27271c5f15
TrueMerc <true_merc@mail.ru>
2022-04-14 21:27:37 +0300

    Fix script

--- src/main/resources/db/migration/V12__insert_device_settings.sql
+++ src/main/resources/db/migration/V12__insert_device_settings.sql
@@ -1,1 +1,4 @@
-UPDATE devices SET settings = '{"baudRate":9600,"parity":"NONE","dataBits":8,"stopBits":"ONE"}'::jsonb WHERE ID = 1;
+UPDATE devices
+SET settings =
+        '{"type":"LaboratoryStandSettings","baudRate":9600,"parity":"NONE","dataBits":8,"stopBits":"ONE"}'::jsonb
+WHERE ID = 1;
